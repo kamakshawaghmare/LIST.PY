c582b28a3f2508e5f7d1884843b01e7268951c78
kamakshawaghmare <102783461+kamakshawaghmare@users.noreply.github.com>
2022-07-16 00:00:38 +0530

    Add files via upload

---
+++ magic square.py
@@ -0,0 +1,76 @@
+
+
+# a=[2,7,6],
+# [9,5,1],
+# [4,3,8]
+# i=0
+# while i<len(a):
+#     j=0
+#     d=[]
+#     while j<len(a[i]):
+#         k=0
+#         sum=0
+#         while k<len(a[i]):
+#             sum=sum+a[j][k]
+#             k=k+1
+#         g=0
+#         sum1=0
+#         while g<len(a[i]):
+#             sum1=sum1+a[j][g]
+#             g=g+1
+#         sum2=0
+#         r=0
+#         while r<len(a[i]):
+#             sum2=sum2+a[j][r]
+#             r=r+1
+#         j=j+1
+#         row=sum,sum1,sum2
+#     i=i+1
+# d.append(row)
+# print(d)
+# i=0
+# g=[]
+# sum=0
+# sum1=0
+# sum2=0
+# while i<len(a):
+#     j=0
+#     while j<len(a[i]):
+#         if j==0:
+#             sum=sum+a[i][j]
+#         elif j==1:  
+#             sum1=sum1+a[i][j]
+#         else:
+#             sum2=sum2+a[i][j]
+#         j=j+1
+#     g.append(sum)
+#     g.append(sum1)
+#     g.append(sum2)
+#     i=i+1
+# print(g)
+# i=0
+# right=0
+# left=0
+# s=[]
+# while i<len(a):
+#     J=0
+#     while j<len(a[i]):
+#         if i==j:
+#             right=right+a[i][j]
+#         if i+j==len(a[i]-1):
+#             left=left+a[i][j]
+#         j=j+1
+#     i=i+1
+# s.append(right)
+# s.append(left)
+# print(s)
+    
+
+
+
+
+
+
+
+
+
